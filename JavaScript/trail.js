e33305700c4d6452b2574a2ba62a7bb34bddbe94
vishwasrm108 <vishwasgowdarm108@gmail.com>
2023-10-05 22:30:43 +0530

    updated

--- JavaScript/trail.js
+++ JavaScript/trail.js
@@ -1,3 +1,14 @@
-var a="hello";
-var b="world";
-console.log(a);
+var a="string";
+console.log(typeof a);
+var eloc;
+console.log(typeof eloc);
+var discount=false
+console.log(typeof discount);
+var emp={
+    id:101,
+    name:"vishwas",
+    salary:45000,
+}
+console.log(typeof emp);
+var size=[1,2,3,];
+console.log(typeof size);
