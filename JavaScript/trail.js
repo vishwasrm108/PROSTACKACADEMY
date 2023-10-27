a11a18136f231c324c8390d68e3748026e6deb30
vishwasrm108 <vishwasgowdarm108@gmail.com>
2023-10-27 20:29:13 +0530

    s

---
+++ JavaScript/trail.js
@@ -0,0 +1,16 @@
+// let emp ={
+//     id:101,
+//     name:"Rahul",
+//     email:"rahul@gmail.com"
+// }
+// let details={
+//     email:"Rahul@pm.com",
+//     sal:45000,
+//     loc:"New Delhi"
+// }
+let a=[101,102,103,104]
+let b=[101,102,103,105]
+
+
+let c=[...a,...b]
+console.log(c)
